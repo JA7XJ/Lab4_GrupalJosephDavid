ca2c7fba6b8422665f7a703be25c80d9b94feb84
Your Name <josephmoscoso70@hotmail.com>
2018-05-18 15:54:44 -0600

    mago

--- Lab4_GrupalJosephDavid/src/lab4_grupaljosephdavid/Mago.java
+++ Lab4_GrupalJosephDavid/src/lab4_grupaljosephdavid/Mago.java
@@ -5,6 +5,7 @@
  */
 package lab4_grupaljosephdavid;
 
+import java.util.Scanner;
 import javafx.scene.paint.Color;
 
 /**
@@ -12,6 +13,7 @@ import javafx.scene.paint.Color;
  * @author Joseph
  */
 public class Mago extends Piezas{
+    public static Scanner sc=new Scanner(System.in);
     private String nombre;
     private int x;
     private int y;
@@ -50,7 +52,20 @@ public class Mago extends Piezas{
     
     @Override
     public void mover() {
-        
+        int x2=0;
+        int y2=0;
+        System.out.println("Ingrese las corrdenadas en x");
+        x2=sc.nextInt();
+        System.out.println("Ingrese las coordenadas en y");
+        y2=sc.nextInt();
+        int abs1=Math.abs(x2-x);
+        int abs2=Math.abs(y2-y);
+        if (abs1==abs2) {
+            y=y2;
+            x=2;
+            
+            
+        }
     }
     
 }
